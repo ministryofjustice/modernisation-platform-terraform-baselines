7a2622f568b1908c5bbbc61afc2462ee99683a10
jakemulley <jakemulley@users.noreply.github.com>
2020-10-15 14:53:28 +0100

    Move CloudTrail to static definitions instead of generation

--- locals.tf
+++ locals.tf
@@ -1,7 +1,5 @@
 locals {
   baselines = [
-    "cloudtrail",
-    "config",
-    "kms"
+    "config"
   ]
 }
---
+++ main.tf
@@ -0,0 +1,4 @@
+module "cloudtrail" {
+  source = "./modules/cloudtrail"
+  tags   = var.tags
+}
