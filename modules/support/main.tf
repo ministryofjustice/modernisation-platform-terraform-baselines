94fc9002641c86f4d55088625d5bb53cf34e534b
jakemulley <jakemulley@users.noreply.github.com>
2020-10-22 11:29:34 +0100

    Create a role for Support service access

---
+++ modules/support/main.tf
@@ -0,0 +1,26 @@
+data "aws_caller_identity" "current" {}
+
+# Create an IAM role for AWS Support service access
+resource "aws_iam_role" "support" {
+  name               = "support"
+  assume_role_policy = data.aws_iam_policy_document.assume-role-policy.json
+  tags               = var.tags
+}
+
+data "aws_iam_policy_document" "assume-role-policy" {
+  version = "2012-10-17"
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "support" {
+  role       = aws_iam_role.support.id
+  policy_arn = "arn:aws:iam::aws:policy/AWSSupportAccess"
+}
