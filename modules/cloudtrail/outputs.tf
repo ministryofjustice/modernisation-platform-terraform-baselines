1f97025f7464a2669fab6f6101b2b7f65122b218
jakemulley <jakemulley@users.noreply.github.com>
2021-02-09 13:08:25 +0000

    Add missing output descriptions for CloudTrail and Config

--- modules/cloudtrail/outputs.tf
+++ modules/cloudtrail/outputs.tf
@@ -1,15 +1,19 @@
 output "cloudwatch_log_group_arn" {
-  value = aws_cloudwatch_log_group.cloudtrail.arn
+  value       = aws_cloudwatch_log_group.cloudtrail.arn
+  description = "CloudTrail CloudWatch log group ARN"
 }
 
 output "log_bucket" {
-  value = module.cloudtrail-log-bucket.bucket
+  value       = module.cloudtrail-log-bucket.bucket
+  description = "Direct aws_s3_bucket resource with all attributes"
 }
 
 output "s3_bucket" {
-  value = module.cloudtrail-bucket.bucket
+  value       = module.cloudtrail-bucket.bucket
+  description = "Direct aws_s3_bucket resource with all attributes"
 }
 
 output "sns_topic_arn" {
-  value = aws_sns_topic.cloudtrail.arn
+  value       = aws_sns_topic.cloudtrail.arn
+  description = "CloudTrail SNS topic ARN"
 }
