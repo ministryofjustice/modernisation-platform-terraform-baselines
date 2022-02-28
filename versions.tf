b604a6cefbb8744b13d82202b2b4a034d322a710
David Sibley <david.sibley@digital.justice.gov.uk>
2022-02-28 10:51:58 +0000

    updated reference to new remote tagged version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.47.0"
+      version = ">= 4.0.0, < 5.0.0"
       configuration_aliases = [
         aws.replication-region,
         aws.ap-northeast-1,
