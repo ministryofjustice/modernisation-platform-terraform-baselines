52db394eedec21f5664e47079cbcee4eea1c1cfb
David Sibley <david.sibley@digital.justice.gov.uk>
2023-06-14 13:18:50 +0100

    bumped AWS provider version

--- modules/access-analyzer/versions.tf
+++ modules/access-analyzer/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.20.0"
+      version = "~> 5.0"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.1"
 }
--- modules/cloudtrail/versions.tf
+++ modules/cloudtrail/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 3.47.0"
+      version               = "~> 5.0"
       configuration_aliases = [aws.replication-region]
     }
   }
