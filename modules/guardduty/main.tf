b90a574434cd971a183561b2ffac0d4826f491ca
SteveLinden <stephen.linden@digital.justice.gov.uk>
2023-09-12 10:33:02 +0100

    Update modules/guardduty/main.tf
    Co-authored-by: ewastempel <52969218+ewastempel@users.noreply.github.com>

--- modules/guardduty/main.tf
+++ modules/guardduty/main.tf
@@ -1,6 +1,4 @@
 resource "aws_guardduty_detector" "default" {
-
-  # checkov check not relevant to us. Want it set for all
   #checkov:skip=CKV2_AWS_3: "Ensure GuardDuty is enabled to specific org/region - This will be applied in each account the baseline will run in and in the all supported regions."
 
   enable                       = true
