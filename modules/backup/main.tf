643c66f3ec33b13fad9659d63995ef9a264ba6a2
SteveLinden <stephen.linden@digital.justice.gov.uk>
2023-09-12 09:05:16 +0100

    Amended some comments as per Ewa's suggestion.

--- modules/backup/main.tf
+++ modules/backup/main.tf
@@ -5,7 +5,7 @@ locals {
 resource "aws_backup_vault" "default" {
 
   # KMS used to encrypt is at an account level so not included here.
-  #checkov:skip=CKV_AWS_166: "Ensure Backup Vault is encrypted at rest using KMS CMK"
+  #checkov:skip=CKV_AWS_166: "Ensure Backup Vault is encrypted at rest using KMS CMK - Tricky to implement, hence using AWS managed KMS key"
 
   name = "everything"
   tags = var.tags
@@ -15,7 +15,7 @@ resource "aws_backup_vault" "default" {
 resource "aws_backup_plan" "default" {
 
   # We use KMS keys from different accounts so this is not relevant to us
-  #checkov:skip=CKV_AWS_166: "Ensure Backup Vault is encrypted at rest using KMS CMK"
+  #checkov:skip=CKV_AWS_166: "Ensure Backup Vault is encrypted at rest using KMS CMK - Tricky to implement, hence using AWS managed KMS key"
 
   name = "backup-daily-retain-30-days"
 
