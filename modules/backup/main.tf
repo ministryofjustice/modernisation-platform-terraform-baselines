7107947b3bdc76b479f0931ae7ac65456ca8e36e
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2024-02-23 10:59:45 +0000

    Added tags as var.

--- modules/backup/main.tf
+++ modules/backup/main.tf
@@ -118,10 +118,7 @@ resource "aws_backup_selection" "non_production" {
 resource "aws_sns_topic" "backup_failure_topic" {
   kms_master_key_id = "alias/aws/sns"
   name              = "backup_failure_topic"
-  tags = {
-    Name        = "Backup failure topic"
-    Description = "This backup topic is so the Modernisation Platform team can subscribe to backup notifications from selected accounts, or teams using member-unrestricted accounts can create their own subscriptions."
-  }
+  tags = var.tags
 }
 
 # Attaches the SNS topic to the backup vault to subscribe for notifications
