0a02d9decb018df96d80db02df56345a86a0c3a6
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2024-09-16 14:54:17 +0100

    test

--- modules/backup/providers.tf
+++ modules/backup/providers.tf
@@ -1,4 +1,9 @@
-# Replica region for kms key
+
+locals {
+  environment_management = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
+}
+
+#Replica region for kms key
 provider "aws" {
   alias  = "modernisation-platform-eu-west-1"
   region = "eu-west-1"
