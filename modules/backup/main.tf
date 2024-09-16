23a1e360dbe11b18a30b6a313079ca658b7d3584
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2024-09-16 11:04:47 +0100

    test

--- modules/backup/main.tf
+++ modules/backup/main.tf
@@ -68,15 +68,16 @@ data "aws_iam_policy_document" "backup-alarms-kms" {
     }
   }
 
-  # Statement allowing specific IAM user to replicate the KMS key
+  # Statement allowing specific IAM user to replicate the KMS key dynamically
   statement {
-    effect    = "Allow"
-    actions   = ["kms:ReplicateKey"]
+    effect = "Allow"
+    actions = ["kms:ReplicateKey"]
     resources = ["*"]
 
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::REDACTED:user/testing-ci"]
+      # Dynamically referencing the IAM user for replication permission
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/testing-ci"]
     }
   }
 }
