33c4b6673771b50e4476aaad5019d6fc14736b0a
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2024-09-16 15:51:21 +0100

    final boss

--- modules/backup/versions.tf
+++ modules/backup/versions.tf
@@ -3,7 +3,9 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+      configuration_aliases = [  aws.eu-west-2,aws.eu-west-1]
     }
   }
   required_version = ">= 1.0.1"
 }
+
--- modules/securityhub-alarms/providers.tf
+++
@@ -1,5 +0,0 @@
-# Replica region for kms key
-provider "aws" {
-  alias  = "modernisation-platform-eu-west-1"
-  region = "eu-west-1"
-}
