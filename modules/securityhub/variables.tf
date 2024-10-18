aaa51d3f2e792c6a9ca23ecfb0cc35206d88892e
Rich Green <richard.green@digital.justice.gov.uk>
2024-10-18 08:46:35 +0100

    add types to variables

--- modules/securityhub/variables.tf
+++ modules/securityhub/variables.tf
@@ -1,14 +1,17 @@
 variable "sechub_eventbridge_rule_name" {
   description = "SecurityHub Eventbridge rule name"
   default     = "sechub_high_and_critical_findings"
+  type        = string
 }
 
 variable "sechub_sns_topic_name" {
   description = "SecurityHub SNS Topic name"
   default     = "sechub_findings_sns_topic"
+  type        = string
 }
 
 variable "sechub_sns_kms_key_name" {
   description = "SecurityHub SNS Topic KMS key name"
   default     = "alias/sns-kms-key"
+  type        = string
 }
