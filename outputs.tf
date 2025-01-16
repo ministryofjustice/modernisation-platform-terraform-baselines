24687989e1f69c06fc7a51fc20b5d368a9e382d9
David Sibley <david.sibley@digital.justice.gov.uk>
2025-01-16 17:47:52 +0000

    refactored output to draw from a map containing the backup module names

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,32 @@
+locals {
+  backup_modules = {
+    "ap-northeast-1" = module.backup-ap-northeast-1
+    "ap-northeast-2" = module.backup-ap-northeast-2
+    "ap-south-1"     = module.backup-ap-south-1
+    "ap-southeast-1" = module.backup-ap-southeast-1
+    "ap-southeast-2" = module.backup-ap-southeast-2
+    "ca-central-1"   = module.backup-ca-central-1
+    "eu-central-1"   = module.backup-eu-central-1
+    "eu-north-1"     = module.backup-eu-north-1
+    "eu-west-1"      = module.backup-eu-west-1
+    "eu-west-2"      = module.backup-eu-west-2
+    "eu-west-3"      = module.backup-eu-west-3
+    "sa-east-1"      = module.backup-sa-east-1
+    "us-east-1"      = module.backup-us-east-1
+    "us-east-2"      = module.backup-us-east-2
+    "us-west-1"      = module.backup-us-west-1
+    "us-west-2"      = module.backup-us-west-2
+  }
+}
+
 output "backup_failure_topics" {
-  value = {
-    "ap-northeast-1" = try(module.backup-ap-northeast-1["enabled"].backup_aws_sns_topic_arn, null)
-    "ap-northeast-2" = try(module.backup-ap-northeast-2["enabled"].backup_aws_sns_topic_arn, null)
-    "ap-south-1"     = try(module.backup-ap-south-1["enabled"].backup_aws_sns_topic_arn, null)
-    "ap-southeast-1" = try(module.backup-ap-southeast-1["enabled"].backup_aws_sns_topic_arn, null)
-    "ap-southeast-2" = try(module.backup-ap-southeast-2["enabled"].backup_aws_sns_topic_arn, null)
-    "ca-central-1"   = try(module.backup-ca-central-1["enabled"].backup_aws_sns_topic_arn, null)
-    "eu-central-1"   = try(module.backup-eu-central-1["enabled"].backup_aws_sns_topic_arn, null)
-    "eu-north-1"     = try(module.backup-eu-north-1["enabled"].backup_aws_sns_topic_arn, null)
-    "eu-west-1"      = try(module.backup-eu-west-1["enabled"].backup_aws_sns_topic_arn, null)
-    "eu-west-2"      = try(module.backup-eu-west-2["enabled"].backup_aws_sns_topic_arn, null)
-    "eu-west-3"      = try(module.backup-eu-west-3["enabled"].backup_aws_sns_topic_arn, null)
-    "sa-east-1"      = try(module.backup-sa-east-1["enabled"].backup_aws_sns_topic_arn, null)
-    "us-east-1"      = try(module.backup-us-east-1["enabled"].backup_aws_sns_topic_arn, null)
-    "us-east-2"      = try(module.backup-us-east-2["enabled"].backup_aws_sns_topic_arn, null)
-    "us-west-1"      = try(module.backup-us-west-1["enabled"].backup_aws_sns_topic_arn, null)
-    "us-west-2"      = try(module.backup-us-west-2["enabled"].backup_aws_sns_topic_arn, null)
+  value = { for key, value in local.backup_modules :
+    key => try(value["enabled"].backup_aws_sns_topic_arn, null)
   }
 }
 
 output "backup_vault_topics" {
-  value = {
-    "ap-northeast-1" = try(module.backup-ap-northeast-1["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "ap-northeast-2" = try(module.backup-ap-northeast-2["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "ap-south-1"     = try(module.backup-ap-south-1["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "ap-southeast-1" = try(module.backup-ap-southeast-1["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "ap-southeast-2" = try(module.backup-ap-southeast-2["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "ca-central-1"   = try(module.backup-ca-central-1["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "eu-central-1"   = try(module.backup-eu-central-1["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "eu-north-1"     = try(module.backup-eu-north-1["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "eu-west-1"      = try(module.backup-eu-west-1["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "eu-west-2"      = try(module.backup-eu-west-2["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "eu-west-3"      = try(module.backup-eu-west-3["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "sa-east-1"      = try(module.backup-sa-east-1["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "us-east-1"      = try(module.backup-us-east-1["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "us-east-2"      = try(module.backup-us-east-2["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "us-west-1"      = try(module.backup-us-west-1["enabled"].backup_vault_lock_sns_topic_arn, null)
-    "us-west-2"      = try(module.backup-us-west-2["enabled"].backup_vault_lock_sns_topic_arn, null)
+  value = { for key, value in local.backup_modules :
+    key => try(value["enabled"].backup_vault_lock_sns_topic_arn, null)
   }
 }
