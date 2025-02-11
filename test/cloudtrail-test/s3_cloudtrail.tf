ff525abb9bdb0700b3b5dd6645980815af75e7a9
David Sibley <david.sibley@digital.justice.gov.uk>
2025-02-11 10:48:09 +0000

    added public access block, set unit test bucket to use prefix to ensure unique name

--- test/cloudtrail-test/s3_cloudtrail.tf
+++ test/cloudtrail-test/s3_cloudtrail.tf
@@ -1,7 +1,16 @@
 resource "aws_s3_bucket" "cloudtrail_s3_bucket" {
-  bucket        = var.cloudtrail_bucket
+  bucket_prefix = var.cloudtrail_bucket
   force_destroy = true
   tags          = local.tags
+}
+
+resource "aws_s3_bucket_public_access_block" "cloudtrail_s3_bucket" {
+  bucket = aws_s3_bucket.cloudtrail_s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "s3_cloudtrail_policy" {
