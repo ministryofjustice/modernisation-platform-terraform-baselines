77033fd5e70ab554ee4046e35776185fb29442c2
mikereiddigital <mike.reid@justice.gov.uk>
2025-05-08 14:33:56 +0100

    Adds a depends_on statement as the module call is doing the lookup before the topic has been re-created.

--- modules/securityhub-alarms/main.tf
+++ modules/securityhub-alarms/main.tf
@@ -697,6 +697,9 @@ resource "aws_cloudwatch_metric_alarm" "orgaccess_role_usage" {
 
 module "pagerduty_high_priority_alerts" {
   source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=0179859e6fafc567843cd55c0b05d325d5012dc4" # v2.0.0
+  depends_on = [
+    aws_sns_topic.high_priority_alarms
+  ]
   sns_topics                = compact([aws_sns_topic.high_priority_alarms.name])
   pagerduty_integration_key = var.high_priority_pagerduty_key
 }
