0b7bdf6cbb07e1da631b083ea5acd07562613312
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2025-06-30 16:15:54 +0100

    🔥 Removed Unused Commented Out Code (#848)

--- modules/cloudtrail/outputs.tf
+++ modules/cloudtrail/outputs.tf
@@ -3,16 +3,6 @@ output "cloudwatch_log_group_arn" {
   description = "CloudTrail CloudWatch log group ARN"
 }
 
-# output "log_bucket" {
-#   value       = module.cloudtrail-log-bucket.bucket
-#   description = "Direct aws_s3_bucket resource with all attributes"
-# }
-
-# output "s3_bucket" {
-#   value       = module.cloudtrail-bucket.bucket
-#   description = "Direct aws_s3_bucket resource with all attributes"
-# }
-
 output "sns_topic_arn" {
   value       = aws_sns_topic.cloudtrail.arn
   description = "CloudTrail SNS topic ARN"
@@ -37,3 +27,4 @@ output "sns_topic_policy_arn" {
 output "cloudtrail_policy_arn" {
   value = aws_iam_policy.cloudtrail.arn
 }
+
