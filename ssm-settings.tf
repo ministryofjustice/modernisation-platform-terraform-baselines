636adf986db8eff8e4878351bfb354f53b4587f7
khatraf <khatra.farah@digital.justice.gov.uk>
2025-10-01 09:29:28 +0100

    removed lifecycle block as baselines module uses version 6 so it's not affected by ARN mismatch

--- ssm-settings.tf
+++ ssm-settings.tf
@@ -8,43 +8,28 @@ resource "aws_ssm_service_setting" "block_ssm_doc_public_sharing_eu_west_1" {
   provider      = aws.eu-west-1
   setting_id    = local.setting_id
   setting_value = local.setting_value
-  lifecycle {
-    ignore_changes = [setting_id]
-  }
 }
 
 resource "aws_ssm_service_setting" "block_ssm_doc_public_sharing_eu_west_2" {
   provider      = aws.eu-west-2
   setting_id    = local.setting_id
   setting_value = local.setting_value
-  lifecycle {
-    ignore_changes = [setting_id]
-  }
 }
 
 resource "aws_ssm_service_setting" "block_ssm_doc_public_sharing_eu_west_3" {
   provider      = aws.eu-west-3
   setting_id    = local.setting_id
   setting_value = local.setting_value
-  lifecycle {
-    ignore_changes = [setting_id]
-  }
 }
 
 resource "aws_ssm_service_setting" "block_ssm_doc_public_sharing_eu_central_1" {
   provider      = aws.eu-central-1
   setting_id    = local.setting_id
   setting_value = local.setting_value
-  lifecycle {
-    ignore_changes = [setting_id]
-  }
 }
 
 resource "aws_ssm_service_setting" "block_ssm_doc_public_sharing_us_east_1" {
   provider      = aws.us-east-1
   setting_id    = local.setting_id
   setting_value = local.setting_value
-  lifecycle {
-    ignore_changes = [setting_id]
-  }
 }
