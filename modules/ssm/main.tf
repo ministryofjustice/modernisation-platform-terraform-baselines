c4dc8761c0660cd6184c38f4e65978ae8c967cdb
khatraf <khatra.farah@digital.justice.gov.uk>
2025-10-07 10:29:48 +0100

    Add lifecycle ignore_changes for SSM service setting_id

--- modules/ssm/main.tf
+++ modules/ssm/main.tf
@@ -1,4 +1,7 @@
 resource "aws_ssm_service_setting" "disable_public_sharing" {
   setting_id    = "/ssm/documents/console/public-sharing-permission"
   setting_value = "Disable"
+    lifecycle {
+    ignore_changes = [setting_id]
+  }
 }
