9325ee4f166d8eb967ab61d1184650b69eee1516
khatraf <khatra.farah@digital.justice.gov.uk>
2026-02-09 11:10:45 +0000

    remove duplicate

--- modules/securityhub/eventbridge-forwarding.tf
+++ modules/securityhub/eventbridge-forwarding.tf
@@ -1,7 +1,10 @@
 data "aws_iam_policy_document" "sechub_forwarding_assume_role" {
+  count = local.forward_securityhub_findings ? 1 : 0
+
   statement {
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
+
     principals {
       type        = "Service"
       identifiers = ["events.amazonaws.com"]
@@ -9,15 +12,8 @@ data "aws_iam_policy_document" "sechub_forwarding_assume_role" {
   }
 }
 
-resource "aws_iam_role" "sechub_forwarding_role" {
-  count              = var.enable_securityhub_event_forwarding && var.central_event_bus_arn != "" ? 1 : 0
-  name               = "sechub-forward-to-central-bus"
-  assume_role_policy = data.aws_iam_policy_document.sechub_forwarding_assume_role.json
-  tags               = var.tags
-}
-
 data "aws_iam_policy_document" "sechub_forwarding_policy" {
-  count = var.enable_securityhub_event_forwarding && var.central_event_bus_arn != "" ? 1 : 0
+  count = local.forward_securityhub_findings ? 1 : 0
 
   statement {
     effect    = "Allow"
@@ -26,18 +22,18 @@ data "aws_iam_policy_document" "sechub_forwarding_policy" {
   }
 }
 
-resource "aws_iam_role_policy" "sechub_forwarding_role_policy" {
-  count  = var.enable_securityhub_event_forwarding && var.central_event_bus_arn != "" ? 1 : 0
-  name   = "sechub-forward-to-central-bus"
-  role   = aws_iam_role.sechub_forwarding_role[0].id
-  policy = data.aws_iam_policy_document.sechub_forwarding_policy[0].json
+resource "aws_iam_role" "sechub_forwarding" {
+  count = local.forward_securityhub_findings ? 1 : 0
+
+  name_prefix        = "SecurityHubForwarding-"
+  assume_role_policy = data.aws_iam_policy_document.sechub_forwarding_assume_role[0].json
+  tags               = var.tags
 }
 
-resource "aws_cloudwatch_event_target" "sechub_findings_central_bus" {
-  for_each = var.enable_securityhub_slack_alerts && var.enable_securityhub_event_forwarding && var.central_event_bus_arn != "" ? toset(var.securityhub_slack_alerts_scope) : []
+resource "aws_iam_role_policy" "sechub_forwarding" {
+  count = local.forward_securityhub_findings ? 1 : 0
 
-  rule      = aws_cloudwatch_event_rule.sechub_findings[each.key].name
-  target_id = "ForwardToCentralEventBus"
-  arn       = var.central_event_bus_arn
-  role_arn  = aws_iam_role.sechub_forwarding_role[0].arn
+  name   = "SecurityHubForwarding"
+  role   = aws_iam_role.sechub_forwarding[0].id
+  policy = data.aws_iam_policy_document.sechub_forwarding_policy[0].json
 }
