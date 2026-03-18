155dc13419d81d76f56bae40270af979c6de5e27
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2026-03-18 13:23:46 +0000

    test

--- test/securityhub-alarms-test/outputs.tf
+++ test/securityhub-alarms-test/outputs.tf
@@ -57,16 +57,6 @@ output "iam_policy_changes_metric_filter_ids" {
   description = "Map of IAM policy change CloudWatch log metric filter IDs keyed by event name"
 }
 
-
-output "iam_policy_changes_metric_filter_ids" {
-  description = "Map of IAM policy change CloudWatch log metric filter IDs keyed by event name. Required when metric filters are created using for_each (one per event) instead of a single resource."
-
-  value = {
-    for event_name, metric_filter in aws_cloudwatch_log_metric_filter.iam-policy-changes :
-    event_name => metric_filter.id
-  }
-}
-
 output "iam_policy_changes_alarm_arn" {
   value       = module.securityhub-alarms-test.iam_policy_changes_alarm_arn
   description = "The ARN of the CloudWatch alarm for IAM policy changes"
