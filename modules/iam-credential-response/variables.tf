9de9707f02fbf0ee62562ba0a2ab138e4a277104
ep-93 <109581241+ep-93@users.noreply.github.com>
2026-03-30 16:06:32 +0100

    Update variables.tf

--- modules/iam-credential-response/variables.tf
+++ modules/iam-credential-response/variables.tf
@@ -2,3 +2,9 @@ variable "pagerduty_integration_key" {
   type        = string
   description = "PagerDuty integration key for IAM exposed credential alerts."
 }
+
+variable "tags" {
+  default     = {}
+  description = "Tags to apply to resources, where applicable"
+  type        = map(any)
+}
