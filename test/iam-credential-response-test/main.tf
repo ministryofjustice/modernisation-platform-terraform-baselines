732fb87cc3e8d3c6753e24a91922c3fe997d7e8e
ep-93 <109581241+ep-93@users.noreply.github.com>
2026-03-31 10:45:18 +0100

    Update main.tf

--- test/iam-credential-response-test/main.tf
+++ test/iam-credential-response-test/main.tf
@@ -1,6 +1,7 @@
 module "iam-credential-response-test" {
   source = "../../modules/iam-credential-response"
 
-  pagerduty_integration_key      = var.pagerduty_integration_key
-  credential_responder_role_name = var.credential_responder_role_name
+  pagerduty_integration_key        = var.pagerduty_integration_key
+  credential_responder_role_name   = var.credential_responder_role_name
+  credential_responder_lambda_name = var.credential_responder_lambda_name
 }
