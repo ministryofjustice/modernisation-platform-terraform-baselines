182e13398b27eb0167d071a3ea5db7318aeb9930
ep-93 <109581241+ep-93@users.noreply.github.com>
2026-03-31 09:16:40 +0100

    Update variables.tf

--- test/iam-credential-response-test/variables.tf
+++ test/iam-credential-response-test/variables.tf
@@ -4,6 +4,12 @@ variable "pagerduty_integration_key" {
   type        = string
 }
 
+variable "credential_responder_role_name" {
+  default     = "credential-responder-lambda"
+  description = "Non-default value used by unit tests to avoid naming collisions"
+  type        = string
+}
+
 variable "tags" {
   default     = {}
   description = "Tags to apply to resources, where applicable"
